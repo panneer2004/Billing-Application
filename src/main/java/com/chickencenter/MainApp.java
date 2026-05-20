e88cfa13df818ae027a8806cd62697a153614c1a
Ajith16102001 <kajithkarthikeyan0018@gmail.com>
2026-05-21 00:01:23 +0530

    Final Commit

--- src/main/java/com/chickencenter/MainApp.java
+++ src/main/java/com/chickencenter/MainApp.java
@@ -1,6 +1,7 @@
 package com.chickencenter;
 
 import com.chickencenter.database.DatabaseInitializer;
+import com.chickencenter.util.LoggerUtil;
 import com.chickencenter.util.ToastManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -15,30 +16,35 @@ public class MainApp extends Application {
     public void init() throws Exception {
         super.init();
         DatabaseInitializer.initialize();
+        LoggerUtil.initialize();
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        LoggerUtil.logInfo("UI", "Starting application UI");
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/chickencenter/ui/main.fxml"));
         Parent mainLayout = loader.load();
-        
+
         StackPane globalRoot = new StackPane();
         globalRoot.getChildren().add(mainLayout);
-        
+
         ToastManager.getInstance().init(globalRoot);
-        
+
         Scene scene = new Scene(globalRoot, 1200, 750);
-        
+
         primaryStage.setTitle("JK Chicken Center - Billing System");
         primaryStage.setScene(scene);
         primaryStage.setResizable(true);
         primaryStage.show();
+
+        LoggerUtil.logInfo("UI", "Application UI started successfully");
     }
 
     @Override
     public void stop() throws Exception {
-        super.stop();
+        LoggerUtil.logShutdown();
         com.chickencenter.database.DatabaseConnection.closeConnection();
+        super.stop();
     }
 
     public static void main(String[] args) {
